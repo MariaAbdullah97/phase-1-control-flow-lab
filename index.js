eec34f46939eb2583ed4d4a3fc50ce8571902838
MariaAbdullah97 <meroo3066@gmail.com>
2022-12-26 14:56:36 +0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(scuberGreetingForFeet){
+
+  if(scuberGreetingForFeet<=400){
+    return "This one is on me!";
+
+  }
+  else if(scuberGreetingForFeet>400 && scuberGreetingForFeet <2000){
+
+    return "That will be twenty bucks.";
+  }
+
+  else if(scuberGreetingForFeet>2000 && scuberGreetingForFeet<2500){
+  
+    return "I will gladly take your thirty bucks.";
+  }
+  else{
+  
+    return "No can do.";
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(ternaryCheckCity){
   // Write your code here!
+
+ return ternaryCheckCity=="NYC"? "Ok, sounds good.": "No go." ;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(switchOnCharmFromTip){
   // Write your code here!
+  switch(switchOnCharmFromTip){
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye."
+  }
 }
+
+
+scuberGreetingForFeet(2600);
+ernaryCheckCity("uu");
